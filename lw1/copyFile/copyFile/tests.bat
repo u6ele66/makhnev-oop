3f71a93cc1751716505f7f933aec53a87b252e07
Andrew Makhnev <andr.xray@mail.ru>
2020-07-10 09:21:07 +0300

    New test.bat file

--- lw1/copyFile/copyFile/tests.bat
+++ lw1/copyFile/copyFile/tests.bat
@@ -3,22 +3,22 @@ rem Переменная PROGRAM будет хранить первый аргумент командной строки заключённы
 set PROGRAM="%~1"
 
 rem проверяем копирование пустого файла (копируем его во временную папку текущего пользователя)
-%PROGRAM% empty.txt C:\Users\Lenovo\source\repos\copyFile\copyFile\empty.txt || goto err
-fc.exe C:\Users\Lenovo\source\repos\copyFile\copyFile\empty.txt empty.txt || goto err
+%PROGRAM% empty.txt C:\Users\Lenovo\Desktop\oop\lw1\copyFile\copyFile\empty.txt || goto err
+fc.exe CC:\Users\Lenovo\Desktop\oop\lw1\copyFile\copyFile\empty.txt\empty.txt || goto err
 echo Test 1 successful 
 
 rem проверяем копирование файла из одной строки
-%PROGRAM% one-line.txt C:\Users\Lenovo\source\repos\copyFile\copyFile\one-line.txt || goto err
-fc.exe C:\Users\Lenovo\source\repos\copyFile\copyFile\one-line.txt one-line.txt || goto err
+%PROGRAM% one-line.txt C:\Users\Lenovo\Desktop\oop\lw1\copyFile\copyFile\one-line.txt || goto err
+fc.exe C:\Users\Lenovo\Desktop\oop\lw1\copyFile\copyFile\one-line.txt\one-line.txt || goto err
 echo Test 2 successful
 
 rem проверяем копирование файла из нескольких строк
-%PROGRAM% multiline.txt C:\Users\Lenovo\source\repos\copyFile\copyFile\multiline.txt || goto err
-fc.exe C:\Users\Lenovo\source\repos\copyFile\copyFile\multiline.txt multiline.txt || goto err
+%PROGRAM% multiline.txt C:\Users\Lenovo\Desktop\oop\lw1\copyFile\copyFile\multiline.txt || goto err
+fc.exe C:\Users\Lenovo\Desktop\oop\lw1\copyFile\copyFile\multiline.txt || goto err
 echo Test 3 successful
 
 rem ожидаем ненулевой код ошибки при копировании несуществующего файла
-%PROGRAM% non-existing-file-name.txt C:\Users\Lenovo\source\repos\copyFile\copyFile\non-existing-file-name.txt || goto err
+%PROGRAM% non-existing-file-name.txt CC:\Users\Lenovo\Desktop\oop\lw1\copyFile\copyFile\non-existing-file-name.txt || goto err
 echo Test 4 successful
 
 echo Program testing succeeded
